428990955797745e94250296b8bd3259e89684db
Askold882 <jactorenss@gmail.com>
2018-10-14 17:03:11 +0300

    express

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
-var serveStatic = require('serve-static');
+var path = require('path');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist"));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port);
