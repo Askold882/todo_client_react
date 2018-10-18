aa69a02bc75fa578f15febf4be6176460f884e00
Askold882 <jactorenss@gmail.com>
2018-10-18 10:56:27 +0300

    path fixes

--- src/resources/resources.js
+++ src/resources/resources.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const base_url = 'https://young-spire-61946.herokuapp.com/';
-// const base_url = 'http://localhost:3000/';
+//const base_url = 'http://localhost:3000/api/';
 
 export function checkToken(params) {
-  return  axios.get(`${base_url}api/sessions?user_token=${params.user_token}`)
+  return  axios.get(`${base_url}sessions?user_token=${params.user_token}`)
 }
 
 export function createSession(params) {
-  return axios.post(`${base_url}api/sessions`, params)
+  return axios.post(`${base_url}sessions`, params)
 }
 
 export function destroySession(params) {
@@ -18,37 +18,37 @@ export function destroySession(params) {
 /////////
 
 export function indexTasks(params) {
-  return axios.get(`${base_url}api/tasks`, { params: params})
+  return axios.get(`${base_url}tasks`, { params: params})
 }
 
 export function showTask(params) {
-  return axios.get(`${base_url}api/tasks/${params.id}`, { params: params})
+  return axios.get(`${base_url}tasks/${params.id}`, { params: params})
 }
 
 export function createTask(params) {
-  return axios.post(`${base_url}api/tasks`, params)
+  return axios.post(`${base_url}tasks`, params)
 }
 
 export function updateTask(params) {
-  return axios.put(`${base_url}api/tasks/${params.id}`,params)
+  return axios.put(`${base_url}tasks/${params.id}`,params)
 }
 
 export function destroyTasks(params) {
-  return axios.delete(`${base_url}api/tasks/${params.id}?id=${params.id}`, {params: params})
+  return axios.delete(`${base_url}tasks/${params.id}?id=${params.id}`, {params: params})
 }
 
 export function paramsTasks(params) {
-  return axios.post(`${base_url}api/tasks?user_token=${params}`)
+  return axios.post(`${base_url}tasks?user_token=${params}`)
 }
 
 /////////
 
 export function createUser(params) {
-  return axios.post(`${base_url}api/users`, params)
+  return axios.post(`${base_url}users`, params)
 }
 
 export function updateUser(params) {
-  return axios.put(`${base_url}api/aplication?user_token=${params.confirm_token}`)
+  return axios.put(`${base_url}aplication?user_token=${params.confirm_token}`)
 }
 
 
